bd3c3b1c709e4450fa1f284b910a1dc9b70e9f1d
puneeeeeeet <puneetchhabra1607@gmail.com>
2026-03-16 12:40:03 +0530

    built corrected

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,21 +1,27 @@
 import "./globals.css";
+import { ReactNode } from "react";
 import { CheckoutProvider } from "../context/CheckoutContext";
 import Navbar from "../components/Navbar";
 
 export const metadata = {
-  title: "Ecoyaan Checkout"
+  title: "Ecoyaan Checkout",
+  description: "Simplified checkout flow built with Next.js",
 };
 
-export default function RootLayout({ children }) {
+export default function RootLayout({
+  children,
+}: {
+  children: ReactNode;
+}) {
   return (
     <html lang="en">
       <body>
-      <CheckoutProvider>
-  <div className="max-w-4xl mx-auto p-6">
-    <Navbar />
-    {children}
-  </div>
-</CheckoutProvider>
+        <CheckoutProvider>
+          <div className="max-w-4xl mx-auto p-6">
+            <Navbar />
+            {children}
+          </div>
+        </CheckoutProvider>
       </body>
     </html>
   );
